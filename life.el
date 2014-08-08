4840565adb5b28e11f2a963ca744ca0e5320c3e8
Kjell Andreassen <andreassen.kjell@gmail.com>
2014-08-08 17:21:47 +0200

    Cycle to the next generation seems to work, the display (insert-xy) has a bug.

--- life.el
+++ life.el
@@ -2,9 +2,6 @@
 (defun alive (world cell)
   (not (null (gethash cell world))))
 
-(defun alive2 (world x y)
-  (alive world (cell-new x y)))
-
 ;; return number of alive neighbours
 (defun neighbours (world cell)
   (reduce '+ (mapcar (lambda (c) (if c 1 0))
@@ -42,17 +39,91 @@
 (defun cell-new (x y)
   `(pos ,(cons x y) age 0))
 
-(defun new-world (cells)
+(defun world-new (cells)
   (let ((world (make-hash-table :test 'equal)))
-    (mapcar (lambda (c) (puthash c c world)) cells)
+    (mapcar (lambda (c) (world-add-cell world c)) cells)
     world))
+
+(defun world-add-cell (world cell)
+  (puthash cell cell world))
 
 ;; does this cell survive to the next cycle?
 (defun survives (world cell)
   (let ((n-neighbours (neighbours world cell)))
-    (if (and (<= n-neighbours 3) (>= n-neighbours 2))
-        t
-      nil)))
+    (and (<= n-neighbours 3) (>= n-neighbours 2))))
+
+;; will this dead cell come to life?
+(defun spawns (world cell)
+  (let ((n-neighbours (neighbours world cell)))
+    (= n-neighbours 3)))
+
+(defun cycle (world)
+  (let ((next-world (world-new '()))
+        (checked-cells (make-hash-table :test 'equal)))
+    (maphash (lambda (cell _alive)
+               (when (survives world cell)
+                 (world-add-cell next-world cell))
+               (puthash cell cell checked-cells)
+               (mapcar (lambda (nc)
+                         (when (null (gethash nc checked-cells))
+                           (puthash nc nc checked-cells)
+                           (when (spawns world nc)
+                             (world-add-cell next-world nc))))
+                       (neighbour-cells cell)))
+             world)
+    next-world))
+
+(defun insert-xy (x y char)
+  (beginning-of-buffer)
+  (let ((next-line-add-newlines t))
+    (dotimes (l y)
+      (next-line)))
+  (dotimes (c x)
+    (if (equal (point) (point-at-eol))
+        (insert " ")
+      (right-char)))
+  (insert-char char)
+  (when (not (point-at-eol))
+    (delete-char 1)))
+
+(defun insert-world (world)
+  (maphash (lambda (_key cell)
+             (insert-xy (cell-x cell) (cell-y cell) ?¤))
+           world))
+
+(defun animate-life (world frame-delay cycles)
+  (let ((buffer "life-buffer"))
+    (with-current-buffer (get-buffer-create buffer)
+      (switch-to-buffer buffer)
+      (overwrite-mode)
+      (dotimes (x cycles)
+        (erase-buffer)
+        (insert-world world)
+        (beginning-of-buffer)
+        (sit-for frame-delay)
+        (setq world (cycle world))
+      ))))
+
+(defun glider ()
+  (world-new (list (cell-new 2 1)
+                   (cell-new 3 2)
+                   (cell-new 1 3) (cell-new 2 3) (cell-new 3 3))))
+
+(defun blinker ()
+  (world-new (list (cell-new 4 2)
+                   (cell-new 4 3)
+                   (cell-new 4 4))))
+
+;; play
+
+(animate-life (glider) 0.1 100)
+(animate-life (blinker) 0.2 1)
+(animate-life (cycle (blinker)) 0.2 1)
+
+(animate-life
+ (world-new (list (cell-new 6 4)
+                  (cell-new 7 4)))
+ 0.2 1)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; tests
@@ -75,24 +146,24 @@
 
 ;; test neighbours
 (test-many
- '((assertEqual 0 (neighbours (new-world '())
+ '((assertEqual 0 (neighbours (world-new '())
                               (cell-new 1 1)))
-   (assertEqual 1 (neighbours (new-world (list (cell-new 0 1)))
+   (assertEqual 1 (neighbours (world-new (list (cell-new 0 1)))
                               (cell-new 1 1)))
-   (assertEqual 2 (neighbours (new-world (list (cell-new 1 1)
+   (assertEqual 2 (neighbours (world-new (list (cell-new 1 1)
                                                (cell-new 2 1)
                                                (cell-new 3 1)))
                               (cell-new 2 1)))
-   (assertEqual 1 (neighbours (new-world (list (cell-new 1 1)
+   (assertEqual 1 (neighbours (world-new (list (cell-new 1 1)
                                                (cell-new 2 1)
                                                (cell-new 3 1)))
                               (cell-new 1 1)))
-   (assertEqual 4 (neighbours (new-world (list (cell-new 1 1)
+   (assertEqual 4 (neighbours (world-new (list (cell-new 1 1)
                                                (cell-new 3 1)
                                                (cell-new 1 3)
                                                (cell-new 3 3)))
                               (cell-new 2 2)))
-   (assertEqual 6 (neighbours (new-world (list (cell-new 1 1)
+   (assertEqual 6 (neighbours (world-new (list (cell-new 1 1)
                                                (cell-new 2 1)
                                                (cell-new 3 1)
                                                (cell-new 1 3)
@@ -103,23 +174,41 @@
 ;; test alive
 (test-many
  '((assertEqual nil
-                (alive (new-world '())
+                (alive (world-new '())
                        (cell-new 0 0)))
    (assertEqual t
-                (alive (new-world (list (cell-new 0 0)))
+                (alive (world-new (list (cell-new 0 0)))
                        (cell-new 0 0)))
    (assertEqual nil
-                (alive (new-world (list (cell-new 0 0)))
+                (alive (world-new (list (cell-new 0 0)))
                        (cell-new 1 1)))))
 
 ;; test survives
 (test-many
  '((assertEqual t
-                (survives (new-world (list (cell-new 1 1)
+                (survives (world-new (list (cell-new 1 1)
                                            (cell-new 2 1)
                                            (cell-new 3 1)))
                           (cell-new 2 1)))
    (assertEqual nil
-                (survives (new-world (list (cell-new 1 1)
+                (survives (world-new (list (cell-new 1 1)
                                            (cell-new 2 1)))
                           (cell-new 2 1)))))
+
+;; test cycle
+(let ((empty-from-one (cycle (world-new (list (cell-new 1 1)))))
+      (empty-from-two (cycle (world-new (list (cell-new 1 1)
+                                              (cell-new 1 2))))))
+  (test-many
+   '(;; blinker
+     (assertEqual t (alive (cycle (blinker)) (cell-new 3 3)))
+     (assertEqual t (alive (cycle (blinker)) (cell-new 4 3)))
+     (assertEqual t (alive (cycle (blinker)) (cell-new 5 3)))
+     (assertEqual 3 (hash-table-count (cycle (blinker))))
+     ;; one cell to empty
+     (assertEqual nil (alive empty-from-one (cell-new 1 1)))
+     (assertEqual 0 (hash-table-count empty-from-one))
+     ;; two cells to empty
+     (assertEqual nil (alive empty-from-two (cell-new 1 1)))
+     (assertEqual 0 (hash-table-count empty-from-two))
+     )))
